acabcc534fb6cceb2f6229b62e08e19317378eab
Xierumeng <xierumeng@hotmail.com>
2024-03-23 22:01:04 -0700

    Python formatting tools (#2)
    * Formatting tools
    
    * PR fixes

--- imaging.py
+++ imaging.py
@@ -1,6 +1,7 @@
 """
 Taking images with a USB camera on a computer on the drone.
 """
+
 import pathlib
 import time
 
@@ -26,38 +27,41 @@ def main() -> int:
 
     camera = cv2.VideoCapture(CAMERA_NAME)
     if not camera.isOpened():
-        with open(SAVE_PREFIX + "error.txt", "w", encoding="utf-8") as f:
-            f.write(SAVE_PREFIX + "\n" + "ERROR: Failed to open camera")
+        with open(f"{SAVE_PREFIX}error.txt", "w", encoding="utf-8") as f:
+            f.write(f"{SAVE_PREFIX}\n" + "ERROR: Failed to open camera\n")
+
         return -1
 
     result = camera.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
     if not result:
-        with open(SAVE_PREFIX + "error.txt", "w", encoding="utf-8") as f:
-            f.write(SAVE_PREFIX + "\n" + "ERROR: Failed to set camera width")
+        with open(f"{SAVE_PREFIX}error.txt", "w", encoding="utf-8") as f:
+            f.write(f"{SAVE_PREFIX}\n" + "ERROR: Failed to set camera width\n")
+
         return -1
 
     result = camera.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
     if not result:
-        with open(SAVE_PREFIX + "error.txt", "w", encoding="utf-8") as f:
-            f.write(SAVE_PREFIX + "\n" + "ERROR: Failed to set camera height")
+        with open(f"{SAVE_PREFIX}error.txt", "w", encoding="utf-8") as f:
+            f.write(f"{SAVE_PREFIX}\n" + "ERROR: Failed to set camera height\n")
+
         return -1
 
     width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
     if width != CAMERA_WIDTH:
-        with open(SAVE_PREFIX + "error.txt", "w", encoding="utf-8") as f:
+        with open(f"{SAVE_PREFIX}error.txt", "w", encoding="utf-8") as f:
             f.write(
-                SAVE_PREFIX + "\n"
-                + "ERROR: Camera width: " + str(width) + ", expected: " + str(CAMERA_WIDTH)
+                f"{SAVE_PREFIX}\n" + f"ERROR: Camera width: {width}, expected: {CAMERA_WIDTH}\n"
             )
+
         return -1
 
     height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
     if height != CAMERA_HEIGHT:
-        with open(SAVE_PREFIX + "error.txt", "w", encoding="utf-8") as f:
+        with open(f"{SAVE_PREFIX}error.txt", "w", encoding="utf-8") as f:
             f.write(
-                SAVE_PREFIX + "\n"
-                + "ERROR: Camera height: " + str(height) + ", expected: " + str(CAMERA_HEIGHT)
+                f"{SAVE_PREFIX}\n" + f"ERROR: Camera height: {height}, expected: {CAMERA_HEIGHT}\n"
             )
+
         return -1
 
     name_counter = 0
@@ -66,7 +70,7 @@ def main() -> int:
         if not result:
             continue
 
-        cv2.imwrite(SAVE_PREFIX + str(name_counter) + ".png", image)
+        cv2.imwrite(f"{SAVE_PREFIX}{name_counter}.png", image)
         name_counter += 1
 
         time.sleep(DELAY)
