0d5218a1e76fbea068e0eea7d5531dc647d679db
Maxwell Lou <maxlou2005@gmail.com>
2024-11-29 21:50:02 -0500

    Fixup test_picam_settings.py

--- test_picam_settings.py
+++ test_picam_settings.py
@@ -1,24 +1,32 @@
 """
 Utility script to test different picamera settings (for use on a Rpi5).
-See appendix C in https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf
-for avilable configs.
+For available settings, see appendix C: https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf
 """
 
 import picamera2
 
-picam2 = picamera2.Picamera2()
 
-picam2.configure(picam2.create_preview_configuration())
-# Example default settings for use with IR camera + IR filter. (only sees IR hotspot)
-# Don't change Brightness (artificially makes everything whiter) or Contrast (doesn't look too good)
-# picam2.set_controls({"ExposureTime": 250, "AnalogueGain": 64.0, "Brightness": 0.0, "Contrast": 1.0})
-print(picam2.controls)
-picam2.start(show_preview=True)
+if __name__ == "__main__":
+    picam2 = picamera2.Picamera2()
 
-while True:
-    command = input("enter a config and value (space separated): ")
-    config = command.split()[0]
-    value = float(command.split()[1])  # pylint: disable=invalid-name
-    if config == "ExposureTime":
-        value = int(value)  # pylint: disable=invalid-name
-    picam2.set_controls({command: value})
+    picam2.configure(picam2.create_preview_configuration())
+    # Example default settings for use with IR camera + IR filter. (only sees IR hotspot)
+    # Don't change Brightness (artificially makes everything whiter) or Contrast (doesn't look too good)
+    # picam2.set_controls({"ExposureTime": 250, "AnalogueGain": 64.0, "Brightness": 0.0, "Contrast": 1.0})
+    print(picam2.controls)
+    picam2.start(show_preview=True)
+
+    while True:
+        command = input("Enter a config and value (e.g. 'ExposureTime 250'): ")
+
+        input_tokens = command.split()
+        if len(input_tokens != 2):
+            print("Invalid input")
+            continue
+
+        config, raw_value = input_tokens
+        value = int(raw_value) if config == "ExposureTime" else float(raw_value)
+
+        picam2.set_controls({command: value})
+        print("New controls set")
+        print(picam2.controls)
